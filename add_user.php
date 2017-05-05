8240c4ab788b69d2a8e4c4632e321ccbf525c267
UnnikrishnanSasikumar <us57@njit.edu>
2017-05-05 19:36:01 -0400

    add user

---
+++ add_user.php
@@ -0,0 +1,42 @@
+<?php
+// Get the product data
+$firstname = filter_input(INPUT_POST, 'firstname', FILTER_VALIDATE_INT);
+$lastname = filter_input(INPUT_POST, 'lastname');
+$email = filter_input(INPUT_POST, 'email');
+$phno = filter_input(INPUT_POST, 'phno');
+$dob = filter_input(INPUT_POST, 'dob');
+$gender = filter_input(INPUT_POST, 'gender');
+$password = filter_input(INPUT_POST, 'password');
+$username = filter_input(INPUT_POST, 'username');
+
+// Validate inputs
+if ($firstname == null || $firstname == false ||
+        $email == null || $password == null || $password == false|| $gender == null ) {
+    $error = "Invalid product data. Check all fields and try again.";
+    include('error.php');
+} else {
+    require_once('database.php');
+
+    // Add the product to the database  
+    $query = 'INSERT INTO useraccount
+                 (firstname, lastname, email, phno, dob, gender, password, username)
+              VALUES
+                 (:firstname, :lastname, :email, :phno, :dob, :gender, :password, : username)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':firstname', $firstname);
+    $statement->bindValue(':lastname', $lastname);
+    $statement->bindValue(':email', $email);
+    $statement->bindValue(':phno', $phno);
+    $statement->bindValue(':dob', $dob);
+    $statement->bindValue(':gender', $gender)
+    ;
+    $statement->bindValue(':password', $password);
+    $statement->bindValue(':username', $username);
+    $statement->execute();
+    $statement->closeCursor();
+
+    // Display the Product List page
+    include('index.php');
+}
+?>
+?>
