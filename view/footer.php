f255ecb77226608e97e57f32cb1c3028a3a7b82d
UnnikrishnanSasikumar <us57@njit.edu>
2017-05-06 12:45:53 -0400

    header and footer files

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p class="copyright">
+        &copy; <?php echo date("Y"); ?> To Do List.
+    </p>
+</footer>
+</body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>To Do List</title>
+   
+
+<!-- the body section -->
+<body>
+<header><h1>To Do List</h1></header>
