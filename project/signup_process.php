cad21ebe2211fc1c4b5b1bb8f8649f6528104061
UnnikrishnanSasikumar <us57@njit.edu>
2017-05-07 16:25:44 -0400

    signup page modification

--- project/signup_process.php
+++ project/signup_process.php
@@ -17,28 +17,28 @@
       
 
 
-<div class="container">
+<div class="container" style="width: 24%;position: relative;center: -40%;background: #6495ED;padding: 8px 10px 8px 12px;color: #FFFAF0;border-radius: 7px;">
   <h2>User Information</h2>
   <form method="post" action="createaccount.php">
     <div class="form-group">
       <!-- <label>Email:</label> -->
-      <input type="text" name="email" class="form-control" id="email" placeholder="Enter email">
+      <input type="text" name="email" class="form-control" id="email" placeholder="Enter email" style="padding: 2px 7px 10px 9px";>
     </div>
     <div class="form-group">
       <!-- <label>Password:</label> -->
-      <input type="password" name="password" class="form-control" id="email" placeholder="Enter password">
+      <input type="password" name="password" class="form-control" id="email" placeholder="Enter password" style="padding: 2px 7px 10px 9px";>
     </div>
     <div class="form-group">
       <!-- <label>First Name:</label> -->
-      <input type="text" name="fname" class="form-control" id="email" placeholder="Enter first name">
+      <input type="text" name="fname" class="form-control" id="email" placeholder="Enter first name" style="padding: 2px 7px 10px 9px";>
     </div>
     <div class="form-group">
       <!-- <label>Last Name:</label> -->
-      <input type="text" name="lname" class="form-control" id="email" placeholder="Enter last name">
+      <input type="text" name="lname" class="form-control" id="email" placeholder="Enter last name" style="padding: 2px 7px 10px 9px";>
     </div>
     <div class="form-group">
       <!-- <label>Phone Number:</label> -->
-      <input type="text" name="phone" class="form-control" id="email" placeholder="Enter phone number">
+      <input type="text" name="phone" class="form-control" id="email" placeholder="Enter phone number" style="padding: 2px 7px 10px 9px";>
     </div>
     <div class="form-group">
       <!-- <label>Date of Birth:</label> -->
