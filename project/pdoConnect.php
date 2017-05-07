848f87f519d366d7d7916f5bbea94b81855dced0
UnnikrishnanSasikumar <us57@njit.edu>
2017-05-07 03:23:14 -0400

    commiting database connection

--- project/pdoConnect.php
+++ project/pdoConnect.php
@@ -1,34 +1,28 @@
 <?php
-//PDO connect
+//DBconnect
 $db = NULL;
-// Let's connect to a database
-// Order of db connection: Heroku mySQL Database --> Local mySQL Database
-// Check to see if we are on a Heroku Server by checking for an environmental variable with db data
+// Connecting to Database
 if ((getenv('JAWSDB_URL') != null)) {
-	
-	$dbparts = parse_url(getenv('JAWSDB_URL'));
+		$dbparts = parse_url(getenv('JAWSDB_URL'));
 	$hostname = $dbparts['host'];
 	$username = $dbparts['user'];
 	$password = $dbparts['pass'];
 	$database = ltrim($dbparts['path'],'/');
-	
-	try {
+		try {
 	    $db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password, array(PDO::MYSQL_ATTR_FOUND_ROWS => true));
-	    // set the PDO error mode to exception
+	    // setting PDO error mode to exception
 	    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	    //echo "Connected successfully";
-    }
+	       }
 	catch(PDOException $e) {
   		http_error(500, "Internal Server Error", "We couldn't connect to a Heroku MySQL database.");
-  		//echo "Connection failed: " . $e->getMessage();
-    }
+  	    }
 }
 else {
 	
 	$hostname = "sql2.njit.edu";
-	$username = "yz746";
-	$password = "XpKmPKQQB";
-	$database = "yz746";
+	$username = "us57";
+	$password = "SNTWAlNug";
+	$database = "us57";
 	try {
 		$db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password, array(PDO::MYSQL_ATTR_FOUND_ROWS => true));
 		
