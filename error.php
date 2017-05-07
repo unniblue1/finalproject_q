20e62d1bf8d890bc16cea4320882066eeff9134d
UnnikrishnanSasikumar <us57@njit.edu>
2017-05-07 02:08:53 -0400

    error info page

---
+++ error.php
@@ -0,0 +1,17 @@
+<?php include 'view/header.php'; ?>
+<!doctype html>
+<html>
+<body>
+	<div id="about" class="container-fluid">
+	  <div class="row">
+	    <div class="col-sm-8">
+	      <h2>Get Started</h2><br>
+	      <h4>The email address doesn't exist or password is not right? Try again please!</h4>
+	      <br><a href="login.php"> Go back to login page</a>
+	      <br>
+	    </div>
+	  </div>
+	</div>
+</body>
+</html>
+<?php include 'view/footer.php'; ?>
