1cff7f9645ffdca2b97b9d2a2570f211952aeade
UnnikrishnanSasikumar <us57@njit.edu>
2017-05-08 13:49:48 -0400

    committing footer file with changes

--- project/view/footer.php
+++ project/view/footer.php
@@ -1,9 +1,9 @@
 <!doctype html>
 <html>
 <body>
-<footer class="container-fluid text-center">
+<footer class="container-fluid text-center" style="width: 24%;position: relative;center: -40%;background: #F0E68C;padding: 8px 10px 8px 12px;color: #DC143C;border-radius: 7px;">
     <p class="copyright" style="color:black;">
-    	<a href="logout.php">Log Out</a><br>
+    	<a href="logout.php">Sign out</a><br>
         &copy; <?php echo date("Y"); ?> Do It Now, Inc.
     </p>
 </footer>
