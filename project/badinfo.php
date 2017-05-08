dc0dfa1544777078d2cbf9877bbfefd535104817
UnnikrishnanSasikumar <us57@njit.edu>
2017-05-08 14:10:43 -0400

    changes to badinfo page

--- project/badinfo.php
+++ project/badinfo.php
@@ -6,7 +6,7 @@
 	  <div class="row">
 	    <div class="col-sm-8">
 	      <h2>Get Started</h2><br>
-	      <h4>The email address doesn't exist or password is not right? Try again please!</h4>
+	      <h4>Your password is Incorrect! Try again.</h4>
 	      <br><a href="login.php"> Go back to login page</a>
 	      <br>
 	    </div>
